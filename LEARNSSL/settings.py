a66afcf6a4af2920dad1582939796698b732310e
Nathan <ntg9vz@virginia.edu>
2017-07-13 14:33:05 -0400

    Thinking emoji

--- LEARNSSL/settings.py
+++ LEARNSSL/settings.py
@@ -122,6 +122,6 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 STATICFILES_DIRS = (os.path.join('static'), )
