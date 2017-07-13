0d62d79dedc0cc6d49694ff3007645e132beb2c9
Nathan <ntg9vz@virginia.edu>
2017-07-13 14:49:53 -0400

    hmm

--- LEARNSSL/settings.py
+++ LEARNSSL/settings.py
@@ -124,4 +124,6 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
-STATICFILES_DIRS = (os.path.join('static'), )
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
