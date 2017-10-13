ad0e8e2c68fe32be27e80719027ff6d05e42cd9e
Forrest Weatherby <forrest.weatherby@ticketstoday.com>
2017-10-13 00:23:14 -0400

    SSL 2 Scenarios

--- lessons/urls.py
+++ lessons/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     url(r'^$', views.LessonHome.as_view(), name='lessons'),
     url(r'^ssl20scenario1/', views.ssl20.as_view(), name='ssl20'),
     url(r'^ssl20alt1/', views.ssl20Alt1.as_view(), name='ssl20alt1'),
-    url(r'^ssl3s1/', views.ssl3S1.as_view(), name='ssl3s1'),
+    url(r'^ssl20s2/', views.ssl20S2.as_view(), name = 'ssl20s2'),
+    url(r'^ssl20s3/', views.ssl20S3.as_view(), name = 'ssl20s3'),
+    url(r'^ssl3s1/', views.ssl3S1.as_view(), name='ssl3s1')
 ]
